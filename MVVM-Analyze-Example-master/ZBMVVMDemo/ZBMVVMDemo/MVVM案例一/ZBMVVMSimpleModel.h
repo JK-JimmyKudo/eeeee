3460efdea2f8445027905e1a9ca18cdc1f11ad45
pengwei <565731968@qq.com>
2022-07-15 11:51:07 +0800

    文件上传

---
+++ MVVM-Analyze-Example-master/ZBMVVMDemo/ZBMVVMDemo/MVVM案例一/ZBMVVMSimpleModel.h
@@ -0,0 +1,16 @@
+//
+//  ZBMVVMSimpleModel.h
+//  ZBMVVMDemo
+//
+//  Created by 帅斌 on 2018/7/30.
+//  Copyright © 2018年 personal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZBMVVMSimpleModel : NSObject
+
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic,strong) NSString *sex;
+
+@end
